5840c82d4051101981c86505c0cab8492bfd0e12
Jzx3 <35195041+jzx3@users.noreply.github.com>
2018-04-06 07:07:29 +0800

    Add research, help text, holidays, merge item cmd

--- prestige/bot.py
+++ prestige/bot.py
@@ -20,24 +20,27 @@ from discord.ext import commands
 import discord
 import os
 import logging
-import pss_prestige as p
 import pss_market as mkt
+import pss_prestige as p
+import pss_research as rs
 import pytz
-import time, datetime
+import time, datetime, holidays
 
 
 # ----- Setup ---------------------------------------------------------
 command_prefix = os.getenv('COMMAND_PREFIX')
 BOT_OWNER = os.getenv("DISCORD_BOT_OWNER")
+BOT_VERSION = '0.02'
 
 tbl, rtbl = p.get_char_sheet()
+df_research_designs = rs.get_research_designs()
 # welcome_txt = """**Welcome to the Pixel Starships Discord!**
 # This is a place where we can interact with devs and players from other alliances/fleets
 # 1. :zipper_mouth: you can mute any channel to avoid notifications from that channel
 # 2. :microphone: #announcements-and-tips-of-the-day are for anything really important on PSS, as well as a means for the devs to post announcements
 # 3. :bulb: if you have any suggestions for the game, you can post them in #suggestion-posts. previously discussed suggestions are in #summaries
 # 4. :beetle: #game-support is for bugs, etc. \@@TheRealTiffany is usually there
-# 5. :robot: We have a #dolores-bot-room. type “`/help`” to find out how to use the bot for character stats/prestige combos, and item prices/stats.
+# 5. :robot: We have a #dolores-bot-room. type “`/help`” to find out how to use this bot for character stats/prestige combos, and item prices/stats.
 # 6. :art: Get a Discord color by posting a screenshot with your trophy level in #screenshots
 # 7. :dove: The usual guidelines for any forum apply (try to be civil, don’t spam, etc)."""
 welcome_txt = """**Welcome to the Pixel Starships Discord!**
@@ -46,11 +49,20 @@ This is a place where we can interact with devs and players from other alliances
 2. 🎤 #announcements-and-tips-of-the-day are for anything really important on PSS, as well as a means for the devs to post announcements
 3. 💡 if you have any suggestions for the game, you can post them in #suggestion-posts. previously discussed suggestions are in #summaries
 4. 🐞 #game-support is for bugs, etc. \@@TheRealTiffany is usually there
-5. 🤖 We have a #dolores-bot-room. type “`/help`” to find out how to use the bot for character stats/prestige combos, and item prices/stats.
+5. 🤖 We have a #dolores-bot-room. type “`/help`” to find out how to use this bot for character stats/prestige combos, and item prices/stats.
 6. 🎨 Get a Discord color by posting a screenshot with your trophy level in #screenshots
 7. 🕊 The usual guidelines for any forum apply (try to be civil, don’t spam, etc)."""
 
+
 # ----- Utilities -----------------------------------------------------
+def get_channel_from_str(server, channel_str):
+    for i, channel in enumerate(server.channels):
+        # txt += '{}: Channel: {} (id={})'.format(i, channel.name, channel.id)
+        if channel.name == channel_str:
+            return channel
+    return None
+
+
 def write_log(log_text, ctx=None):
     if os.path.isfile("debug") is False:
         return
@@ -85,14 +97,29 @@ async def on_command_error(err, ctx):
         await bot.send_message(ctx.message.channel, 'Error: {}'.format(err))
 
 
+@bot.event
+async def on_member_join(member):
+    print('MEMBER JOINED {} | {} Joined: {}'.format(member.name, member.id, member.server))
+    if member.server == 'Pixel Starships':
+        await bot.send_message(member, welcome_txt)
+
+
+@bot.event
+async def on_reaction_add(reaction, user):
+    write_log('Reaction {} added by {}'.format(reaction, user))
+
+
 @commands.cooldown(rate=12, per=120, type=commands.BucketType.channel)
-@bot.command()
+@bot.command(brief='Ping the server to verify that it\'s listening for commands',
+             description='Ping the server to verify that it\'s listening for commands')
 async def ping():
     await bot.say('Pong!')
 
 
 @commands.cooldown(rate=12, per=120, type=commands.BucketType.channel)
-@bot.command(pass_context=True)
+@bot.command(pass_context=True,
+    brief='Get the prestige combinations of the character specified',
+    description='Get the prestige combinations of the character specified')
 async def prestige(ctx, *, char_name=None):
     if char_name is None:
         help_txt = 'Enter: {}prestige [character name]'.format(command_prefix)
@@ -113,7 +140,9 @@ async def prestige(ctx, *, char_name=None):
 
 
 @commands.cooldown(rate=12, per=120, type=commands.BucketType.channel)
-@bot.command(pass_context=True)
+@bot.command(pass_context=True,
+    brief='Get the prestige recipes of the character specified',
+    description='Get the prestige recipes of the character specified')
 async def recipe(ctx, *, char_name=None):
     if char_name is None:
         help_txt = 'Enter: {}recipe [character name]'.format(command_prefix)
@@ -134,7 +163,9 @@ async def recipe(ctx, *, char_name=None):
 
 
 @commands.cooldown(rate=12, per=120, type=commands.BucketType.channel)
-@bot.command(pass_context=True)
+@bot.command(pass_context=True,
+    brief='Get a list of items recently posted on the market',
+    description='Get a list of items recently posted on the market')
 async def market(ctx):
     market_txt = mkt.get_market_data()
     try:
@@ -145,51 +176,53 @@ async def market(ctx):
 
 
 @commands.cooldown(rate=12, per=120, type=commands.BucketType.channel)
-@bot.command(pass_context=True)
+@bot.command(pass_context=True, brief='Get the average price in bux of the item(s) specified.',
+             description='Get the average price in bux of the item(s) specified, as returned by the PSS API. ' +
+             'Note that prices returned by the API may not reflect the real market value, ' +
+             'due to transfers between alts/friends')
 async def price(ctx, *, item_name=None):
     if item_name is None:
         await bot.say('Enter: {}price [item name]'.format(command_prefix))
-    else:
-        write_log(command_prefix + 'price {}'.format(item_name), ctx)
-        raw_text = mkt.load_item_design_raw()
-        rtbl = mkt.parse_item_designs(raw_text)
+        return
+
+    write_log(command_prefix + 'price {}'.format(item_name), ctx)
+    raw_text = mkt.load_item_design_raw()
+    rtbl = mkt.parse_item_designs(raw_text)
+
+    real_name = mkt.get_real_name(item_name, rtbl)
+    if real_name is not None:
         market_txt = mkt.filter_item_designs(item_name, rtbl, filter='price')
-        if market_txt is None:
-            await bot.say('Could not find {}'.format(item_name))
-        else:
-            await bot.say(market_txt)
+        await bot.say(market_txt)
+    else:
+        await bot.say('{} not found'.format(item_name))
 
 
 @commands.cooldown(rate=12, per=120, type=commands.BucketType.channel)
-@bot.command(pass_context=True)
+@bot.command(name='stats', aliases=['item'], pass_context=True,
+             brief='Get the stats of a character or item',
+             description='Get the stats of a character/crew or item')
 async def stats(ctx, *, name=None):
     if name is None:
         await bot.say('Enter: {}stats [name]'.format(command_prefix))
         return
 
+    # First try to find a character match
     write_log(command_prefix + 'stats {}'.format(name), ctx)
     result = p.get_stats(name, embed=False)
-    if result is None:
-        await bot.say('Could not find {}'.format(name))
-    else:
+    if result is not None:
         await bot.say(result)
+        found_match = True
 
-
-@commands.cooldown(rate=12, per=120, type=commands.BucketType.channel)
-@bot.command(pass_context=True)
-async def item(ctx, *, name=None):
-    if name is None:
-        await bot.say('Enter: {}item [name]'.format(command_prefix))
-        return
-
-    write_log(command_prefix + 'item {}'.format(name), ctx)
+    # Next try to find an item match
     raw_text = mkt.load_item_design_raw()
     rtbl = mkt.parse_item_designs(raw_text)
     market_txt = mkt.filter_item_designs(name, rtbl, filter='stats')
-    if market_txt is None:
-        await bot.say('Could not find {}'.format(name))
-    else:
+    if market_txt is not None:
         await bot.say(market_txt)
+        found_match = True
+
+    if found_match is False:
+        await bot.say('Could not find {}'.format(name))
 
 
 # @bot.command(pass_context=True)
@@ -206,7 +239,10 @@ async def item(ctx, *, name=None):
 
 
 @commands.cooldown(rate=12, per=120, type=commands.BucketType.channel)
-@bot.command(pass_context=True)
+@bot.command(pass_context=True,
+    brief='Get the best enhancement item for a given slot',
+    description='Get the best enhancement item for a given slot. ' +
+        'If multiple matches are found, matches will be shown in descending order')
 async def best(ctx, slot=None, enhancement=None):
     if slot is None:
         txt = 'Enter: {}best [slot] [enhancement]'.format(command_prefix)
@@ -230,40 +266,67 @@ async def best(ctx, slot=None, enhancement=None):
         await bot.say(txt)
 
 
-def get_channel_from_str(server, channel_str):
-    for i, channel in enumerate(server.channels):
-        # txt += '{}: Channel: {} (id={})'.format(i, channel.name, channel.id)
-        if channel.name == channel_str:
-            return channel
-    return None
+@commands.cooldown(rate=12, per=120, type=commands.BucketType.channel)
+@bot.command(pass_context=True,
+    brief='Get research data',
+    description='Get the research details on a specific research. ' +
+        'If multiple matches are found, only a brief summary will be provided')
+async def research(ctx, *, research=None):
+    if research is None:
+        txt = 'Enter: {}research [research]'.format(command_prefix)
+        await bot.say(txt)
+        return
+    txt = command_prefix + 'research {}'.format(research)
+    write_log(txt, ctx)
+
+    df_selected = rs.filter_researchdf(df_research_designs, research)
+    txt = rs.research_to_txt(df_selected)
+    if txt is None:
+        await bot.say("No entries found for '{}'".format(research))
+    else:
+        await bot.say(txt)
 
 
 @commands.cooldown(rate=1, per=600, type=commands.BucketType.channel)
-@bot.command(pass_context=True, hidden=True)
-async def welcome(ctx):
+@bot.command(pass_context=True, hidden=True,
+    brief='Send the welcome message to a specific channel',
+    description='Send the welcome message to a specific channel')
+async def welcome(ctx, channel='general'):
     write_log(command_prefix + 'welcome', ctx)
     # await bot.say("message content", embed=your_embed)
     # await bot.send_message(destination, welcome_txt, embed=your_embed)
     # await bot.send_message(ctx.message.channel, welcome_txt)
-    dest_channel = get_channel_from_str(ctx.message.server, 'general')
+    dest_channel = get_channel_from_str(ctx.message.server, channel)
     if dest_channel is not None:
         await bot.send_message(dest_channel, welcome_txt)
     else:
-        print('Could not find channel: #general')
+        print('Could not find channel: #{}'.format(channel))
+        await bot.say('Could not find channel: #{}'.format(channel))
 
 
 @commands.cooldown(rate=12, per=120, type=commands.BucketType.channel)
-@bot.command(pass_context=True)
+@bot.command(pass_context=True, brief='Get the day and time in Melbourne, Australia',
+             description='Get the day and time in Melbourne, Australia. Gives the ' +
+             'name of the Australian holiday, if it is a holiday in Australia.')
 async def time(ctx):
-    mel_tz = pytz.timezone('Australia/Melbourne')
     now = datetime.datetime.now()
+    mel_tz = pytz.timezone('Australia/Melbourne')
     mel_time = now.replace(tzinfo=pytz.utc).astimezone(mel_tz)
-    str_time = 'Time in Melbourne: {}'.format(mel_time.strftime('%H:%M'))
+    str_time = mel_time.strftime('It is %A, %H:%M in Melbourne')
+
+    aus_holidays = holidays.Australia(years=now.year, prov='ACT')
+    # now = datetime.date(now.year, now.month, now.day)
+    mel_time = datetime.date(mel_time.year, mel_time.month, mel_time.day)
+    if mel_time in aus_holidays:
+        str_time += '\nIt is also a holiday ({}) in Australia'.format(aus_holidays[mel_time])
     await bot.say(str_time)
 
 
+# @commands.is_owner()
 @commands.cooldown(rate=24, per=120, type=commands.BucketType.channel)
-@bot.command(pass_context=True, hidden=True)
+@bot.command(pass_context=True, hidden=True,
+    brief='These are testing commands, usually for debugging purposes',
+    description='These are testing commands, usually for debugging purposes')
 async def testing(ctx, *, action=None):
     if action == 'refresh':
         write_log(command_prefix + 'testing refresh', ctx)
@@ -282,11 +345,37 @@ async def testing(ctx, *, action=None):
     elif action == 'info':
         write_log(command_prefix + 'testing info', ctx)
         server = ctx.message.server
-        txt = 'Server: {}\n'.format(server)
-        txt += 'Message Author: {}\n'.format(ctx.message.author)
-        txt += 'This Channel: {}\n'.format(ctx.message.channel)
+        author = ctx.message.author
+        channel = ctx.message.channel
+        txt = 'Server: {} (id={})\n'.format(server, server.id)
+        txt += 'Message Author: {} (id={})\n'.format(author, author.id)
+        txt += 'This Channel: {} (id={})\n'.format(channel, channel.id)
         txt += 'Default Channel: {}\n'.format(server.default_channel)
+        txt += 'Discord.py version: {}'.format(discord.__version__)
         await bot.say(txt)
+    await bot.delete_message(ctx.message)
+
+
+@commands.cooldown(rate=1, per=60, type=commands.BucketType.channel)
+@bot.command(pass_context=True, hidden=True, brief='Gives the version number of the bot',
+    description='Gives the version number of the bot')
+async def version(ctx):
+    write_log(command_prefix + 'version', ctx)
+    txt = 'Bot version is {}'.format(BOT_VERSION)
+    await bot.say(txt)
+
+
+@commands.cooldown(rate=1, per=60, type=commands.BucketType.channel)
+@bot.command(pass_context=True, hidden=True, brief='Makes the bot say something',
+    description='Makes the bot say something. Only the bot owner is allowed to ' +
+                'use this command.')
+async def say(ctx, *, text=None):
+    write_log(command_prefix + 'say {}'.format(text), ctx)
+    if text is None:
+        return
+    if str(ctx.message.author) == BOT_OWNER:
+        await bot.say(txt)
+    await bot.delete_message(ctx.message)
 
 
 if __name__ == '__main__':
